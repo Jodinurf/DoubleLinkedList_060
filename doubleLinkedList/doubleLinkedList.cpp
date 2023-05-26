1e787727a891950dcde6d58945ebaae8b81b4839
Jodinurf <jodinurfarkhani04@gmail.com>
2023-05-26 15:50:08 +0700

    membuat input dan tampilan dari nama dan nim mahasiswa pada method addNode di class DoubleLinkedList

--- doubleLinkedList/doubleLinkedList.cpp
+++ doubleLinkedList/doubleLinkedList.cpp
@@ -25,3 +25,13 @@ public:
 	void searchData();
 
 };
+
+void DoubleLinkedList::addNode() {
+	int nim;
+	string nm;
+	cout << "\nEnter the roll number of the student : ";
+	cin >> nim;
+	cout << "\nEnter the name of the student : ";
+	cin >> nm;
+
+}
